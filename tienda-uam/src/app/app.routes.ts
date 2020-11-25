80ef81b1707c31df6b77c17f3cfabbfe3968b918
MiguelArias7 <miguel.ariasg@autonoma.edu.co>
2020-11-25 00:46:23 -0500

    Se agrega redireccionamiento al componente home desde el componente navigation-menu

--- tienda-uam/src/app/app.routes.ts
+++ tienda-uam/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: '**', pathMatch:'full', redirectTo: 'routePath' }
 ];
