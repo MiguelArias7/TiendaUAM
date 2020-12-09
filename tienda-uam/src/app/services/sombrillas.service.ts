00f8dcf50849c85a0bf85396b3f6f2ef63873df1
Andres Ladino <monkey.programmer7@gmail.com>
2020-12-09 13:30:02 -0500

    impermeable

--- tienda-uam/src/app/services/sombrillas.service.ts
+++ tienda-uam/src/app/services/sombrillas.service.ts
@@ -12,7 +12,7 @@ export class SombrillasService {
       img:"assets/images/imgSombrillas/azul.jpg",
       name:"sombrilla azul",
       ref:"S-001",
-      description:"hipermeable",
+      description:"impermeable",
       precio:"1000"
 
      },
@@ -20,35 +20,35 @@ export class SombrillasService {
       img:"assets/images/imgSombrillas/blanca.jpg",
       name:"sombrilla blanca",
       ref:"S-002",
-      description:"hipermeable",
+      description:"impermeable",
       precio:"1000"
 
      }, {
       img:"assets/images/imgSombrillas/negra.jpg",
       name:"sombrilla negra",
       ref:"S-003",
-      description:"hipermeable",
+      description:"impermeable",
       precio:"1000"
 
      }, {
       img:"assets/images/imgSombrillas/rosa.jpg",
       name:"sombrilla rosa",
       ref:"S-004",
-      description:"hipermeable",
+      description:"impermeable",
       precio:"1000"
 
      }, {
       img:"assets/images/imgSombrillas/roja.jpg",
       name:"sombrilla roja",
       ref:"S-005",
-      description:"hipermeable",
+      description:"impermeable",
       precio:"1000"
 
      }, {
       img:"assets/images/imgSombrillas/verde.jpg",
       name:"sombrilla verde",
       ref:"S-006",
-      description:"hipermeable",
+      description:"impermeable",
       precio:"1000"
 
      },
